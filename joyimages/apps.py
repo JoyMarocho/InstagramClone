801856aab4e7c00d6abf4c51dfd8b5f5574813d1
JoyMarocho <joymarocho@gmail.com>
2022-06-05 23:43:37 +0300

    creating urls

--- joyimages/apps.py
+++ joyimages/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class JoyimagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'joyimages'
+
+    def ready(self):
+        import joyimages.signals 
---
+++ joyimages/urls.py
@@ -0,0 +1,20 @@
+from django.urls import re_path as url, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import ImageCreateView,ImageDetailView,ImageListView
+
+
+urlpatterns = [
+    #url(r'^$', views.index, name='index'),
+    url(r'register', views.register_new_user, name='register'),
+    url(r'login', views.login_user, name='login'),
+    url(r'logout', views.logout_user, name='logout'),
+    url(r'new/image/$', ImageCreateView.as_view(), name='image-add'),
+    url(r'(?P<pk>\d+)', ImageDetailView.as_view(), name='image-detail'),
+    url(r'', ImageListView.as_view(), name='image-list'),
+    #url(r'comment/add', CommentCreateView.as_view(), name='author-add'),
+    ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
