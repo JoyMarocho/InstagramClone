1ae7b9fa9a421c49b8f88bd3331036264dbd0b8f
JoyMarocho <joymarocho@gmail.com>
2022-06-05 23:52:53 +0300

    troubleshooting bootstrap

--- joyinsta/settings.py
+++ joyinsta/settings.py
@@ -42,16 +42,16 @@ INSTALLED_APPS = [
     
     'joyimages.apps.JoyimagesConfig',
     'cloudinary',
-    'bootstrap4',
+    'bootstrap5',
     'crispy_forms',
-    'crispy_bootstrap4',
+    'crispy_bootstrap5',
     
 ]
 
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
+CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 
 
