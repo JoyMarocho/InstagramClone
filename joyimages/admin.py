0600f6660eac13e739e85b00d33d1f09c84c6b46
JoyMarocho <joymarocho@gmail.com>
2022-06-05 23:39:02 +0300

    creating the admin

--- joyimages/admin.py
+++ joyimages/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Image,Profile,Comment
 
 # Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('name','posted_on')
+    list_filter = ('name',)
+    search_fields = ['name','caption']
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post','user','body','posted_on','active')
+    list_filter = ('active', 'posted_on')
+    search_fields = ('user', 'body')
+    actions = ['approve_comments']
+
+def approve_comments(self, request, queryset):
+    queryset.update(active=True)
+
+
+admin.site.register(Profile)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Comment, CommentAdmin)
