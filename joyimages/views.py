eb1d041e4602bd68fa29dcce0eb732e02f18111d
JoyMarocho <joymarocho@gmail.com>
2022-06-06 13:06:40 +0300

    creating a logut fucnction

--- joyimages/views.py
+++ joyimages/views.py
@@ -44,3 +44,27 @@ def index(request):
         messages.error(request, 'Unsuccessful registration. Invalid information.')
     form = NewUserForm()
     return render(request, 'registration/registration_form.html', {"registration_form": form} )
+
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username,password=password)
+    if user is not None:
+        login(request, user)
+        messages.info(request, f"You are now logged in as {username}.")
+        return redirect('index')
+        else:
+        messages.error(request, f'Invalid Username or password')
+    else:
+            messages.error(request, f'Invalid username or password')
+    form = AuthenticationForm()
+    return render(request, 'registration/login.html', {"login_form": form})
+
+def logout_user(request):
+    logout(request)
+    messages.info(request, f'You have successfully logged out.')
+    return redirect('index')
